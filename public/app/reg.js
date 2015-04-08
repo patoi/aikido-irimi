0d00c89e3e96bfc05070d916d3f4ef472b35e1ea
István Pató <istvan.pato@gmail.com>
2015-04-08 13:46:06 +0200

    radio button kezelo

--- public/app/reg.js
+++ public/app/reg.js
@@ -5,10 +5,13 @@ var app = angular.module('Reg', []);
 app.controller('RegisztracioCtrl',
   function(RegisztracioService) {
     var reg = this;
+    reg.mkdeTag = '1';
+    reg.penznem = 'huf';
+
     console.log('RegisztracioCtrl');
 
     reg.regisztracio = function() {
-      console.log('Regisztracio nev: ', reg.nev);
+      console.log('Regisztracio: ', reg);
       RegisztracioService.validate(reg);
     }
   }
