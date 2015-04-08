6242d374404da33d2b53b7f06b55f67b134fb9d2
István Pató <istvan.pato@gmail.com>
2015-04-08 21:22:22 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var express = require('express');
+var config = require('./config_eles');
+var app = express();
+var fs = require('fs')
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var winston = require('winston');
+var nodemailer = require('nodemailer');
+var Datastore = require('nedb'),
+  dbReg = new Datastore({
+    filename: __dirname + '/reg.db',
+    autoload: true
+  });
+
+var smtpTransport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: config.email.from,
+    pass: config.email.psw
+  }
+});
+var mailOptions = {
+  from: 'xxx@gmail.com',
+  to: 'xxx@gmail.hu',
+  subject: 'Test',
+  text: 'req.query.text'
+}
+
+// eventlog
+var eventLogStream = fs.createWriteStream(__dirname + '/event.log', {
+  flags: 'a'
+});
+// app log
+winston.add(winston.transports.File, {
+  filename: __dirname + '/reg.log',
+  handleExceptions: true
+});
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(morgan('combined', {
+  stream: eventLogStream
+}));
+
+app.route('/api/regisztraciok')
+  .post(function(req, res) {
+    winston.info('regisztracio in', req.body);
+    // FIXME: egyediseg ellenőrzések, összefüggés ellenőrzések és validálás
+    // FIXME: transzformációk, pl: Capitalize
+    // FIXME: regisztrációs szám generálása
+    dbReg.insert(req.body);
+    // visszakeresés az ID miatt
+    dbReg.find({
+      'nev': req.body.nev
+    }, function(err, docs) {
+      var result = docs[docs.length - 1];
+      winston.info('regisztracio out', result);
+      res.json(result);
+    });
+  })
+  .get(function(req, res) {
+    if (req.query.key === config.adminKey) {
+      dbReg.find({}, function(err, docs) {
+        smtpTransport.sendMail(mailOptions, function(error, response) {
+          if (error) {
+            console.log(error);
+            res.end("smtp error");
+          } else {
+            console.log("Message sent: " + response.message);
+            res.send(docs);
+          }
+        });
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
+app.listen(3000, function() {
+  console.log('Listen on port 3000');
+});
