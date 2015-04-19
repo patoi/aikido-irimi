55fe69c35ed4e8ea57afe366e97f53760955cc8e
István Pató <istvan.pato@gmail.com>
2015-04-19 20:21:25 +0200

    wrong karma conf.

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
             //'public/js/materialize.js',
             'public/app/**/*.module.js',
             'public/app/**/*.service.js',
-            'node_components/angular-mocks/angular-mocks.js',
+            'node_modules/angular-mocks/angular-mocks.js',
             'tests/unit/*.test.js',
         ],
         // list of files / patterns to exclude
