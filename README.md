eed8680ab7d39463ac5b6a1bdfb7b08f1019935b
István Pató <istvan.pato@gmail.com>
2015-04-19 20:07:58 +0200

    add ci env

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 [![Build Status](https://travis-ci.org/patoi/aikido-irimi.svg?branch=master)](https://travis-ci.org/patoi/aikido-irimi)
-
 # aikido-irimi
 Aikido 2015 nyári tábor - regisztráció
---
+++ config.ci.js
@@ -0,0 +1,12 @@
+var config = {}
+config.adminKey = '1234';
+config.email = {};
+config.email.from = 'x1234.x1234@gmail.com';
+config.email.psw = 'xxxxx'
+
+config.email.subject = 'Aikido regisztráció 2015'
+config.email.bcc = 'x1234@x1234x1234-12341234.hu'
+
+config.deadlineDiscount = false;
+
+module.exports = config;
