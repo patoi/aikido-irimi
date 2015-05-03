1a63b9e79f132c0b6c06048727adddcafd4b59a0
István Pató <istvan.pato@gmail.com>
2015-05-03 20:36:34 +0200

    remove unnecessary config

--- config/config.ci.js
+++ config/config.ci.js
@@ -4,7 +4,6 @@ config.port = 4000;
 config.adminKey = '1234';
 config.email = {};
 config.email.from = 'x1234.x1234@gmail.com';
-config.email.psw = 'xxxxx';
 config.email.subject = 'Aikido regisztráció 2015';
 config.email.bcc = 'x1234@x1234x1234-12341234.hu';
 
